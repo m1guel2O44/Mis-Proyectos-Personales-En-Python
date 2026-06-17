b5683d0f878779db4023ebb862005d707158e2c9
M1guel <sergiomiguelcastillorobles@gmail.com>
2026-06-16 21:21:13 -0400

    Add files via upload

---
+++ Cajero_Miguel.py
@@ -0,0 +1,35 @@
+print("Bienvenido al Banco Digital de Miguel\n".title())
+contraseña = "Lia"
+saldo = 1000
+
+for pene in contraseña in range(1):
+    contra = input("Ingrese su contraseña: ")
+if contra != contraseña:
+        print("Contraseña incorrecta, intente de nuevo\n")
+
+
+print("Bienvenido, ingresaste la contraseña correctamente\n".title())
+
+while True:
+    Banco = int(input("¿Que desea hacer?\n 1. Ver Saldo\n 2. Retirar Saldo\n 3. Depositar Saldo\n 4. Salir\n - "))
+    if Banco == 1:
+        print(f"Su saldo actual es: {saldo}")
+    elif Banco == 2:
+        Retirar = int(input("Ingrese el monto que desea retirar: "))
+        if Retirar <= saldo:
+            print(f"Haz retirado: {Retirar}")
+            saldo -= Retirar
+        else:
+            print("El monto a retirar es mayor al de la cuenta\n".title())
+    elif Banco == 3:
+        Depositar = int(input("Ingrese el monto que desea depositar: "))
+        if Depositar > 0:
+            saldo += Depositar
+            print(f"Haz depositado: {Depositar}\n")
+        else:
+            print("El monto a depositar debe ser mayor que cero\n".title())
+    elif Banco == 4:
+        print("Gracias por utilizar el Banco Digital de Miguel".title())
+        break
+    else:
+        print("Opción no válida. Ingrese una opción válida".title())
